e1119ad2d91afda038429ead7b40c1c2e740d4be
itselliexo <elliemjoliver@gmail.com>
2025-02-16 13:43:10 +0000

    Camera follow script

---
+++ ToTheMoonGJ/Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [Header("References")]
+    [SerializeField] private GameObject player;
+
+    [Header("Settings")]
+    private Vector3 playerOffset;
+
+    void Start()
+    {
+        if (player == null)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+
+            if (player == null)
+            {
+                Debug.LogError("No player found in scene");
+            }
+        }
+        playerOffset = new Vector3(0, 1, -10);
+    }
+
+    void LateUpdate()
+    {
+        if (player != null)
+        {
+            Vector3 targetPosition = new Vector3(player.transform.position.x + playerOffset.x, player.transform.position.y + playerOffset.y, transform.position.z);
+
+            transform.position = targetPosition;
+        }
+    }
+}
